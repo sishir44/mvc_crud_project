fd5be6b859fd19b3376917e9afd4470375d930d4
sishir44 <sishirbohara4@gmail.com>
2023-08-30 22:04:33 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,103 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]  /*Get data from Database and display in list*/
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcDemoDbContext.Employee.ToListAsync();
+            return View(employees);
+        }
+
+        [HttpGet] /* Fill data in form */
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] /*Send data to databse*/
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department
+            };
+            await mvcDemoDbContext.Employee.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet] /* get data from dbs for update*/
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await mvcDemoDbContext.Employee.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(employee != null)
+            {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    DateOfBirth = employee.DateOfBirth,
+                    Department = employee.Department
+                };
+                return await Task.Run(() => View("View",viewModel));
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]  /*send update data to database*/
+        public async Task<IActionResult> View(UpdateEmployeeViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employee.FindAsync(model.Id);
+
+            if(employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Department = model.Department;
+
+                await mvcDemoDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost] /* To delete the employee */
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employee.FindAsync(model.Id);
+            if(employee != null)    
+            {
+                mvcDemoDbContext.Employee.Remove(employee);
+                await mvcDemoDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    
+    
+    }
+}
---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext // MVCDemoDbContext is inherit from DbContext which is coming from Microsoft.EntityFrameworkCore
+    {
+        // this is a constructor for class -> MVCDemoDbContext 
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
